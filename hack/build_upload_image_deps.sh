54bb7b48842a973bec1dc52f153280179fd02d82
Frank A. Zdarsky <fzdarsky@redhat.com>
2021-01-25 21:53:16 +0100

    Add sha256 verification, fix xz decompression
    Signed-off-by: Frank A. Zdarsky <fzdarsky@redhat.com>

---
+++ hack/build_upload_image_deps.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+REGISTRY=$(oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}')
+podman login -u $(oc whoami | sed 's/://') -p $(oc whoami -t) --tls-verify=false ${REGISTRY}
+
+IMAGES=$(find images/* -type d | xargs) 
+for i in ${IMAGES} do
+    podman build -t ${REGISTRY}/edt/$(basename ${i}):latest ${i}
+    podman push --tls-verify=false ${REGISTRY}/edt/$(basename ${i}):latest
+done
